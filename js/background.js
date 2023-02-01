34fbd416b5597e2e73b9b6a7901a1e877eecd4f0
tourecodeur <116043808+tourecodeur@users.noreply.github.com>
2023-02-01 23:31:51 +0000

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,9 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('home'), // Required
+    path: "https://assets2.lottiefiles.com/packages/lf20_ijpmnwcu.json", // Required
+    renderer: 'svg', // Required
+    loop: true, // Optional
+    autoplay: true, // Optional
+    // name: "Hello World", // Name for future reference. Optional.
+    
+  })
